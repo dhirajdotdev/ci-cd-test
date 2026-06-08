d1b8c1059932733ca998f76491e70c354262008a
Dhiraj <cforcode69@gmail.com>
2026-06-08 23:13:09 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req, res) => {
   return res.json({
-    message: "Server is up version 2.0.0",
+    message: "Server is up version 2.0.1",
     system: {
       platform: os.platform(),
       arch: os.arch(),
